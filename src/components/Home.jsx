d62caefba40530d9634345704affa0b837a4dca9
paras mittal <mittalparas588@gmail.com>
2024-08-23 17:09:24 +0530

    Done

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Sidebar from './Sidebar.jsx';
+import Video from './Video';
+import { useAuth } from '../context/AuthProvider.jsx';
+import ListItems from './Listitems.jsx';
+
+function Home() {
+  const { data ,loading} = useAuth();
+
+  return (
+    <div className='flex mt-20'>
+      <Sidebar />
+
+    <div className='h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden '>
+        <ListItems/>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5'>
+        {!loading && data.map((item) => {
+        
+          if (item.type !== 'video') return false;
+          return <Video key={item.id} video={item?.video} />;
+        })}
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Home;
